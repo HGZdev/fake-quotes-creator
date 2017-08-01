8fa2254b98d549d88127eb65157b6116a681830e
HGZprogramowanie <HGZ.programowanie@gmail.com>
2017-08-01 21:34:59 +0200

    footer added, new styling, other minor changes

--- src/modules/authors.js
+++ src/modules/authors.js
@@ -1,3 +1,14 @@
-const authors = ["[Wybierz autora...]", "Anonim", "Oscar Wilde", "Marek Tulliusz Cyceron", "Immanuel Kant", "Jan Paweł II", "Konfucjusz", "Paulo Coelho", "Francisco Goya", "Leonardo da Vinci"];
+const authors = [
+  "[Wybierz autora...]",
+  "Anonim",
+  "Oscar Wilde",
+  "Marek Tulliusz Cyceron",
+  "Immanuel Kant",
+  "Jan Paweł II",
+  "Konfucjusz",
+  "Paulo Coelho",
+  "Francisco Goya",
+  "Leonardo da Vinci"
+];
 
-export {authors};
+export { authors };
